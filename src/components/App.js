0ff471ef7554582490e528724519da4a755c182e
LeeYoHan <iyohan@LeeYoHanui-MacBookPro.local>
2019-03-11 17:44:16 +0900

    Create LICENSE - yohan

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello React Boiler Plate</h1>
+        <h1>Hello React</h1>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -12,11 +12,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
-        }
+        }        
       },
       {
         test: /\.css$/,
